a23c327413086f89d1afe3c96e02cc5a9b0e2690
yougottafree <yougottafree@gmail.com>
2022-04-16 13:44:01 -0700

    changed valid program

--- result.py
+++ result.py
@@ -1,10 +1,6 @@
-# this will do the while loop
-i = 0
-while i < 3:
-	y = 0
-	while y < 3:
-		print(y, end="")
-		y = y + 1
-	i = i + 1
-# this will go to the newline
-print("\n", end="")
+# this will do the if else statement
+thisFunny = True
+if thisFunny == True:
+	print(":)", end="")
+else:
+	print("hahahahaha", end="")
